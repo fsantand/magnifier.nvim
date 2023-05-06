7cff64dca4136ef8a45b2fca20dc49de86b95c98
Federico Santander <federicosantander95@gmail.com>
2023-05-05 23:48:57 -0400

    Add setup function

--- lua/magnifier/init.lua
+++ lua/magnifier/init.lua
@@ -1,9 +1,9 @@
-local M = {
-  is_active = false
-}
+local M = {}
+
+M.enabled = false
 
 local function toggle_magnifier(opts, is_active)
- opts.is_active = is_active
+ M.enabled = is_active
 end
 
 local function add_to_scope()
@@ -16,14 +16,18 @@ local function remove_from_scope()
 end
 
 local function list_scope()
-  print(M.is_active)
+  print(M.enabled)
 end
 
-vim.api.nvim_create_user_command("MagnifierEnable", function(_) toggle_magnifier(M, true) end, {})
-vim.api.nvim_create_user_command("MagnifierDisable", function(_) toggle_magnifier(M, false) end, {})
-vim.api.nvim_create_user_command("MagnifierDisable", function(_) toggle_magnifier(M, not M.is_active) end, {})
-vim.api.nvim_create_user_command("MagnifierAdd", add_to_scope, {})
-vim.api.nvim_create_user_command("MagnifierRemove", remove_from_scope, {})
-vim.api.nvim_create_user_command("MagnifierList", list_scope, {})
+function M.setup (opts)
+  M.enabled = opts.enabled_by_default or true
+
+  vim.api.nvim_create_user_command("MagnifierEnable", function(_) toggle_magnifier(M, true) end, {})
+  vim.api.nvim_create_user_command("MagnifierDisable", function(_) toggle_magnifier(M, false) end, {})
+  vim.api.nvim_create_user_command("MagnifierDisable", function(_) toggle_magnifier(M, not M.is_active) end, {})
+  vim.api.nvim_create_user_command("MagnifierAdd", add_to_scope, {})
+  vim.api.nvim_create_user_command("MagnifierRemove", remove_from_scope, {})
+  vim.api.nvim_create_user_command("MagnifierList", list_scope, {})
+end
 
 return M
